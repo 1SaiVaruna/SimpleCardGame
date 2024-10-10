09b7a69acda04ebfa28956668e3527432cd90cfb
1SaiVaruna <sai030804@gmail.com>
2024-10-10 10:20:27 +0100

    Added player class with hand management, card adding, and hand displaying functionality.

--- SimpleCardGame/Player.cs
+++ SimpleCardGame/Player.cs
@@ -8,5 +8,36 @@ namespace SimpleCardGame
 {
     internal class Player
     {
+        private string name;
+        private List<Card> hand;
+
+        public Player(string name)
+        {
+            this.name = name;
+            hand = new List<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            hand.Add(card);
+        }
+
+        public void ShowHand()
+        {
+            Console.WriteLine($"{name}'s hand: ");
+            foreach (Card card in hand)
+            {
+                Console.WriteLine(card);
+            }
+        }
+
+        public void Score()
+        {
+            int score = 0;
+            foreach (Card card in hand)
+            {
+                score += card.Value();
+            }
+        }
     }
 }
