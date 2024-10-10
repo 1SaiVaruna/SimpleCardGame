e944880cb8127e2f2cd5549b1d4a2957787b3e52
1SaiVaruna <sai030804@gmail.com>
2024-10-10 10:16:08 +0100

    Added Deck class with card initialization, shuffling, and drawing functionality.

--- SimpleCardGame/Deck.cs
+++ SimpleCardGame/Deck.cs
@@ -8,5 +8,39 @@ namespace SimpleCardGame
 {
     internal class Deck
     {
+
+        private List<Card> cards;
+        public Deck() 
+        {
+            cards = new List<Card>();
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    cards.Add(new Card(suit, rank));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random random = new Random();
+            cards = cards.OrderBy(x => random.Next()).ToList();
+        }
+
+        public Card DrawCard()
+        {
+            if (cards.Count > 0)
+            {
+                Card drawnCard = cards[0];
+                cards.RemoveAt(0);
+                return drawnCard;
+            }
+
+            else
+            {
+                return null;
+            }
+        }
     }
 }
