d27d6c760a5da214ba586cdccf6a8381d3e07971
1SaiVaruna <sai030804@gmail.com>
2024-10-10 10:11:21 +0100

    Added class with 'suit, rank, and value calculation' functionality

--- SimpleCardGame/Card.cs
+++ SimpleCardGame/Card.cs
@@ -8,5 +8,43 @@ namespace SimpleCardGame
 {
     internal class Card
     {
+        private Suit suit;
+        private Rank rank;
+
+        public Card(Suit suit, Rank rank)
+        {
+            this.suit = suit;
+            this.rank = rank;
+        }
+
+        public Suit Suit
+        {
+            get { return suit; }
+        }
+
+        public Rank Rank
+        {
+            get { return rank; }
+        }
+
+        public override string ToString()
+        {
+            return $"{rank} of {suit}";
+        }
+
+        public int Value()
+        {
+            int value = (int)rank;
+
+            if (value > 10)
+            {
+                value = 10;
+            }
+
+            return value;
+        }
+
+
+
     }
 }
