917b1420437b5960e8537ee880e89665c12ada06
1SaiVaruna <sai030804@gmail.com>
2024-10-10 10:05:54 +0100

    Add project files.

---
+++ SimpleCardGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleCardGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
