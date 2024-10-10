1a826ce47f34c392fa404e1424e3aed3db7c8748
1SaiVaruna <sai030804@gmail.com>
2024-10-10 10:25:15 +0100

    Added main program code to initialize deck, shuffle cards, and simulate a card draw for two players with their scores.

--- SimpleCardGame/Program.cs
+++ SimpleCardGame/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Deck deck = new Deck();
+            deck.Shuffle();
+
+            Player player1 = new Player("Player 1");
+            Player player2 = new Player("Player 2");
+
+            for(int i = 0; i < 2; i++)
+            {
+                player1.AddCard(deck.DrawCard());
+                player2.AddCard(deck.DrawCard());
+            }
+
+            player1.ShowHand();
+            player1.Score();
+
+            player2.ShowHand();
+            player2.Score();
+
+            Console.ReadKey();
         }
     }
 }
